ec83f955454b3e18c24f8818e2d451bdd68d0cb2
ReaganCn <rgncharana2@gmail.com>
2020-08-14 19:55:31 +0300

    added custom animation to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,18 @@ module.exports = {
   theme: {
     fontFamily: {
       'opensans': ['Open Sans'],
-          }
+          },
+    extend: {
+      keyframes: {
+        fadein: {
+          '0%': {color: 'black'},
+          '100%': {color: 'blue'}
+        }
+      },
+      animation: {
+        fadein: 'fadein 3s ease-in-out'
+      }
+    }
   },
   variants: {},
   plugins: [],
