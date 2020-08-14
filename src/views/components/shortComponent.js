2123f62b395fb71c37dd5912f104e903d28ede21
ReaganCn <rgncharana2@gmail.com>
2020-08-14 19:56:31 +0300

    updated the style of shorting component

--- src/views/components/shortComponent.js
+++ src/views/components/shortComponent.js
@@ -2,35 +2,39 @@ import React from "react";
 
 const ShortComponent = (props) => {
   return (
-    <div className="w-11/12 flex flex-col mt-16 justify-center align-middle border rounded p-12 mx-auto">
-      <div className="self-center">
-        <form className="flex-row self-start" onSubmit={props.onSubmit}>
+    <div className="w-9/12 flex flex-col mt-16 border rounded p-12 mx-auto">
+      <div className="">
+        <form className=" flex flex-row self-start h-10 justify-between" onSubmit={props.onSubmit}>
           <input
             type="text"
             name="url"
             id="original url"
             placeholder="Enter URL"
-            className="border border-purple-400 outline-none p-1"
+            className="h-full border border-purple-400 border-t-0 border-r-0 border-l-0 outline-none p-1 w-10/12"
             onChange={props.handleChange}
             value={props.input.url}
           />
-          <button className="border bg-purple-800 w-32 text-white p-1" type="submit">
+          <button
+            className=" h-full border bg-purple-700 w-2/12 text-white p-1 rounded"
+            type="submit"
+          >
             Create
           </button>
         </form>
         <div className="flex-row self-start">
-          <p
-            type="text"
-            name="shorturl"
-            className="outline-none p-1 text-black"
-           
-          >Your new link: {props.input.shorturl}</p>
-          <button className="border bg-purple-800 w-32 text-white p-1">
-            Custom
-          </button>
-          <button className="border bg-purple-800 w-32 text-white p-1">
-            Copy
-          </button>
+
+          <div className="flex flex-col md:flex-row justify-between border mb-1 border-purple-200 rounded pl-2 md:p-0 mt-3">
+            <p className="md:self-center md:ml-2">www.google.com</p>
+            <a className="md:self-center font-semibold text-blue-600" href={`link0.ga/${props.input.shorturl}`} target="_blank">link0.ga/{props.input.shorturl}</a>
+            <div>
+              <button className="w-32 text-purple-600 p-1 h-10  font-bold my-0">
+                Copy
+              </button>
+              <button className="w-32 text-purple-600 p-1 h-10  font-bold rounded rounded-l-none">
+                Customize
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
