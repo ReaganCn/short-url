69d59d8ce7b216ccfedf1386ce2b95d8eb18e029
ReaganCn <rgncharana2@gmail.com>
2020-08-16 21:52:48 +0300

    added react router dom npm package

--- src/app.js
+++ src/app.js
@@ -1,32 +1,28 @@
 import React, { Fragment } from "react";
-import {connect} from "react-redux";
-
+import { connect } from "react-redux";
 
 import SignInConnected from "./views/containers/SignInContainer";
 import ShortConnected from "./views/containers/ShortContainer";
 
 const mapStateToProps = ({ app }) => {
-    return {
-      state: app,
-    };
+  return {
+    state: app,
   };
-  
-  const mapDispatchToProps = (dispatch) => {
-    return {
-      
-    };
-  };
+};
 
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
 
 const App = (props) => {
-return (
-<Fragment>
-    <SignInConnected />
-    <ShortConnected />
-</Fragment>
-)
-}
+  return (
+    <Fragment>
+      <SignInConnected />
+      <ShortConnected />
+    </Fragment>
+  );
+};
 
-const AppConnected = connect(mapStateToProps, mapDispatchToProps)(App)
+const AppConnected = connect(mapStateToProps, mapDispatchToProps)(App);
 
 export default AppConnected;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
 import "../public/styles.css";
 import AppConnected from "./app";
@@ -8,9 +9,11 @@ import store from "./redux/rootStore";
 
 const Main = (props) => {
   return (
-    <Provider store={store}>
-      <AppConnected />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <AppConnected />
+      </Provider>
+    </BrowserRouter>
   );
 };
 
