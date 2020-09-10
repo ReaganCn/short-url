7c1df40ef5043df56644ea94fa56c9bebfe2dd08
Reagan Charana <rgncharana2@gmail.com>
2020-09-10 12:25:36 +0300

    added and styled account menu

--- src/views/components/HomeComponent.js
+++ src/views/components/HomeComponent.js
@@ -7,13 +7,23 @@ const HomeComponent = (props) => {
       className="w-full"
       //     style={{backgroundImage: "url('public/imgs/HeaderImage.jpg')"}}
     >
-      <div className="flex justify-between" id="menu">
-        <span className="text-white font-semibold m-1 ml-2 font-opensans mt-3">
+      <div className="flex justify-between mx-5" id="menu">
+        <span className="text-white font-semibold ml-2 font-opensans mt-3 cursor-pointer hover:text-purple-200">
           About
         </span>
-        <span className="text-white font-semibold m-1 mr-2 font-opensans mt-3">
+        <div className="flex mt-3 flex-col hover:text-purple-200">
+        <span className="text-white font-semibold mr-2 font-opensans cursor-pointer hover:text-purple-200">
           {props.greeting}, {props.info.firstName}
         </span>
+        <i className="fa fa-caret-down text-white mx-auto -mt-1 text-xs"></i>
+        <div className="relative ml-10">       
+         <div id="account-menu" className="flex flex-col z-10 bg-white font-opensans h-20 w-11/12 justify-between p-2 rounded text-center shadow-xl absolute left-0">
+            <span className="w-full text-darkviolet cursor-pointer hover:bg-darkviolet self-center rounded mx-auto hover:text-white">Profile Settings&nbsp;&nbsp;&nbsp;<i className="fa fa-caret-right"></i></span>
+            <span className=" w-full mx-auto text-purple-600 font-bold cursor-pointer hover:bg-darkviolet hover:text-white rounded">Logout&nbsp;&nbsp;&nbsp;<i className="fa fa-sign-out"></i></span>
+        </div></div>
+
+        </div>
+
       </div>
       <div id="logo" className="w-4/12 mx-auto flex items-end">
         <img src="public/imgs/logo3.png" className="inline-block xl:max-w-lg"/>
